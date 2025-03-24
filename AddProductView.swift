84b9698ba10fc4ca450e94e3e401a49d8a098e51
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-03-24 11:48:42 -0400

    Adding save product button functionality

--- AddProductView.swift
+++ AddProductView.swift
@@ -27,6 +27,12 @@ struct AddProductView: View {
                         .keyboardType(.decimalPad)
                     TextField("Input Product Provider", text: $productProvider)
                 }
+                Section {
+                    Button("Save Product") {
+                        saveProduct()
+                    }
+                    .disabled(productName.isEmpty || productDescription.isEmpty || productPrice.isEmpty || productProvider.isEmpty)
+                }
             }
         }
     }
