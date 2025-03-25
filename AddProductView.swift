22f8553e032fea150743b4f3142c0ed666693b6d
gill-jaskiran <156838272+gill-jaskiran@users.noreply.github.com>
2025-03-25 15:16:33 -0400

    Adding comments to AddProductView

--- AddProductView.swift
+++ AddProductView.swift
@@ -9,9 +9,12 @@ import SwiftUI
 import CoreData
 
 struct AddProductView: View {
+    // Getting core Data context
     @Environment(\.managedObjectContext) private var viewContext
+    // dimissing view after saving
     @Environment(\.dismiss) private var dismiss
-
+    
+    // Adding @State variables to store user inputs
     @State private var productName = ""
     @State private var productDescription = ""
     @State private var productPrice = ""
@@ -19,8 +22,10 @@ struct AddProductView: View {
 
     var body: some View {
         NavigationView {
+            // implementing the form UI
             Form {
                 Section(header: Text("Product Details")) {
+                    // Displaying all the input fields
                     TextField("Product Name ", text: $productName)
                     TextField("Product Description", text: $productDescription)
                     TextField("Product Price - CAD", text: $productPrice)
@@ -29,15 +34,19 @@ struct AddProductView: View {
                 }
 
                 Section {
+                    // Adding button to save product
                     Button("Save Product") {
                         saveProduct()
                     }
+                    // button disabled if imput fields are empty - prevents gaps in data storage
                     .disabled(productName.isEmpty || productDescription.isEmpty || productPrice.isEmpty || productProvider.isEmpty)
                 }
             }
+            // adding title to add product section
             .navigationTitle("Add Product")
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
+                    // adding cancel button and dismiss without saving if pressed
                     Button("Cancel") {
                         dismiss()
                     }
@@ -46,23 +55,24 @@ struct AddProductView: View {
         }
     }
     
+    // Adding function to save the product to the core data
     private func saveProduct() {
         let context = viewContext
-
+        // creating new data object
         let newProduct = Product(context: context)
         newProduct.productID = UUID().uuidString
         newProduct.productName = productName
         newProduct.productDescription = productDescription
         newProduct.productProvider = productProvider
-
+        // converting the price into a double
         if let price = Double(productPrice) {
             newProduct.productPrice = price
         }
-
         do {
-            try context.save()
+            try context.save() // saving data to the core data & dismissing view
             dismiss()
         } catch {
+            // Error handling if it fails to save product
             print("Error: Cannot save product: \(error.localizedDescription)")
         }
     }
